dfaf211786ed93193988e4d082dfa7aee9aada4b
caijing666 <2645741934@qq.com>
2019-08-05 20:12:59 +0800

    note

---
+++ dog.java
@@ -0,0 +1,7 @@
+package animal;
+
+public class dog {
+public void shout() {
+	System.err.println("ÍôÍôÍô");
+}
+}
---
+++ test.java
@@ -0,0 +1,10 @@
+package dog2;
+
+import animal.dog;
+
+public class test {
+public static void main(String[] args) {
+	dog d=new dog();
+	d.shout();
+}
+}
